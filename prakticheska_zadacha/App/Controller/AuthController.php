3906718677c47d658c410eb575684bea968ad219
dilianadineva <mnyaha@gmail.com>
2025-05-11 22:52:05 +0300

    Update AuthController.php

--- prakticheska_zadacha/App/Controller/AuthController.php
+++ prakticheska_zadacha/App/Controller/AuthController.php
@@ -1,5 +1,8 @@
 <?php
 class AuthController {
+    public function __construct($mysqli = null) {
+        //simple login provided for now
+    }
     public function login() {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $username = $_POST['username'];
